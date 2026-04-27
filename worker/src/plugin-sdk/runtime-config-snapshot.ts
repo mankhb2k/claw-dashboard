97d798d4b392b27a14aa3312dbfdd5e1248b108f
Mankhb2k <mankhb2k@gmail.com>
2026-04-27 21:53:41 +0700

    chore: ignore worker folder (now separate repo)

--- worker/src/plugin-sdk/runtime-config-snapshot.ts
+++
@@ -1,6 +0,0 @@
-export {
-  clearRuntimeConfigSnapshot,
-  getRuntimeConfigSnapshot,
-  setRuntimeConfigSnapshot,
-} from "../config/runtime-snapshot.js";
-export type { OpenClawConfig } from "../config/types.js";
